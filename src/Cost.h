128a363f8f790e9d31e50813b0303a7a7d523895
Daniel Sundfeld <sund@unb.br>
2015-08-14 11:34:46 +0200

    Cost: explicit public access

--- src/Cost.h
+++ src/Cost.h
@@ -1,16 +1,17 @@
 #ifndef _COST_H
 #define _COST_H
 class Cost {
-    enum {
-        gap = 0,
-        mismatch = 1,
-        match = 2,
+    public:
+        enum {
+            gap = 0,
+            mismatch = 1,
+            match = 2,
 
-        base_paired = 2,
-        unpaired = 0,
-    };
-    static int base_score(const char &a, const char &b); //'alpha' score
-    static int match_score(const char &a, const char &b); // 'beta' score
-    static int compensation_score(const char &a1, const char &a2, const char &b1, const char &b2); // 'tau' score
+            base_paired = 2,
+            unpaired = 0,
+        };
+        static int base_score(const char &a, const char &b); //'alpha' score
+        static int match_score(const char &a, const char &b); // 'beta' score
+        static int compensation_score(const char &a1, const char &a2, const char &b1, const char &b2); // 'tau' score
 };
 #endif
