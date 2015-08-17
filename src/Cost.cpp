045d8cbe68f421653b514621a4cda13f9aa2ab1f
Daniel Sundfeld <sund@unb.br>
2015-08-17 16:04:11 +0200

    Cost: fixup on scores

--- src/Cost.cpp
+++ src/Cost.cpp
@@ -20,21 +20,8 @@ int Cost::match_score(const char &a, const char &b)
 
 int Cost::compensation_score(const char &a1, const char &a2, const char &b1, const char &b2)
 {
-    /*
-     * The score function is alpha + beta + tao
-     * alpha (a): base_score
-     * beta (b): match_score
-     * tao (t): compensation_score
-     */
-
-    // If equal, a = match, b = base_pair, t = zero
-    if (match_score(a1, b1) == match && match_score(a2, b2) == match)
-        return 0;
-    
-    /* They are different but they are basepaired and have the same structure
-       a = mismatch, b = base_pair, t = match - mismatch (compensate) */
-    if (base_score(a1, a2) == base_paired && base_score(b1, b2) == base_paired)
-        return match - mismatch;
-
+    // Give a score if they have the same structure
+    if (base_score(a1, a2) == base_score(b1, b2))
+        return 2 * match; //4 nucleotides, 2 matches...
     return 0; // Otherwise, no donuts for you
 }
