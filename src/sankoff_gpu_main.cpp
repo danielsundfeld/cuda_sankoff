04a54d5bd34353860daa85bdce34a31b53e63df4
Daniel Sundfeld <sund@unb.br>
2016-04-14 16:42:59 -0300

    Sankoff_GPU: Removing useless header and objects

--- src/sankoff_gpu_main.cpp
+++ src/sankoff_gpu_main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <omp.h>
 
-#include "Foldalign.h"
 #include "Sankoff_GPU.h"
 
 int main(int argc, char *argv[])
@@ -9,10 +8,7 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv;
     Sankoff_GPU fold_instance("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCAAAAGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG", "GAUCGUAGUACUGAUGAUCGUAGCAUGGUCAUGCAGUACGUUGACGUCAGUCGUUGAUGCGUACUGAGCUGUACGUCAUG");
-    //Sankoff fold_instance("CCCCCCCCAAAAGGGGGGGG", "GUGCCAACAUUAGUUGGCAC");
-    //Foldalign fold_instance("CCCCCCCCAAAAGGGGGGGG", "GUGCCAACAUUAGUUGGCAC", 20, 20);
-    //return fold_instance.fold_align();
+    //Sankoff_GPU fold_instance("CCCCCCCCAAAAGGGGGGGG", "GUGCCAACAUUAGUUGGCAC");
 
-    //omp_set_num_threads(4);
     return fold_instance.diag_sankoff();
 }
