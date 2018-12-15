31ddf50538e15a6825a7d546f3518de53b318dd6
Daniel Sundfeld <danielsundfeld@gmail.com>
2018-12-15 00:25:38 -0200

    Sequences for GPU with 32 GB

---
+++ test/RF02707.fasta_16_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p RM
+#SBATCH --ntasks-per-node 28
+#SBATCH -t 5:00:00
+SEQ="./seqs/RFAM/RF02707.fasta"
+CMD="./bin/sankoff"
+OPT="-t 16"
+OUT="cpu16"
+
+module avail cuda
+module load cuda
+set -x
+
+#run GPU program
+cd $HOME"/hpc_foldalign"
+
+strace -ve wait4 /usr/bin/time -v $CMD $OPT $SEQ >> $SEQ.$OUT.output 2>&1
