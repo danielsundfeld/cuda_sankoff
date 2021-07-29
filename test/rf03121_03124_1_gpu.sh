bb4fe9febc056ae1153668fb2159e3674034ad26
Daniel Sundfeld <daniel.sundfeld@gmail.com>
2021-07-29 18:53:09 +0000

    run fixup

--- test/rf03121_03124_1_gpu.sh
+++ test/rf03121_03124_1_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 nvidia-smi -a > rf03121_03124_1.fasta.out 2>&1
-/usr/bin/time ./bin/cuda_sankoff ./seqs/sars_cov_2/rf03121_03124/rf03121_03124_1.fasta >> rf03121_03124_1.fasta.out 2>&1
+/usr/bin/time -v ./bin/cuda_sankoff ./seqs/sars_cov_2/rf03121_03124/rf03121_03124_1.fasta >> rf03121_03124_1.fasta.out 2>&1
 aws s3 cp rf03121_03124_1.fasta.out  s3://cudasankoffresults/
 sudo shutdown -h now
